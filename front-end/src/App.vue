828854b1e4521e6c884a9afd16fabf0cf7bcd77e
Tamás Németh <tomuwhu@gmail.com>
2018-09-23 20:09:17 +0200

    .

--- front-end/src/App.vue
+++ front-end/src/App.vue
@@ -3,10 +3,17 @@
     <v-container>
       <v-spacer/>
       <v-layout row>
-        <v-flex v-for="(elem,key) in t[0]">{{key}}</v-flex>
+        <v-flex xs3
+          v-for="(elem,key) in t[0]"
+          class="cell b">{{key}}</v-flex>
       </v-layout>
-      <v-layout row v-for="row in t">
-        <v-flex v-for="(elem,key) in row">{{elem}}</v-flex>
+      <v-layout
+          row
+          v-for="row in t" >
+        <v-flex xs3
+          v-for="(elem,key) in row"
+          class="cell">{{elem}}
+        </v-flex>
       </v-layout>
       <v-spacer/>
     </v-container>
@@ -31,5 +38,13 @@ export default {
 </script>
 
 <style>
-
+.b {
+  font-weight: bold;
+}
+.cell {
+  text-align: center;
+  margin:3px;
+  padding:3px;
+  box-shadow: 1px 1px 4px;
+}
 </style>
