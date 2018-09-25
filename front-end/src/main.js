f6a244c9308351f536dc684c77836e825442829b
Tamás Németh <tomuwhu@gmail.com>
2018-09-25 16:48:34 +0200

    .

--- front-end/src/main.js
+++ front-end/src/main.js
@@ -4,7 +4,7 @@ import './plugins/axios'
 import App from './App.vue'
 import 'roboto-fontface/css/roboto/roboto-fontface.css'
 import 'material-design-icons-iconfont/dist/material-design-icons.css'
-import {idb} from './mymodules/idb.js'
+import { idb } from './mymodules/idb.js'
 
 Vue.config.productionTip = false
 
